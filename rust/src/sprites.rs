3e4b4b659234d7b183d3fb0506ed9d6b723d6431
Jack Bradach <jack@bradach.net>
2021-05-24 21:45:55 -0700

    Implements loading files into surfaces.

--- rust/src/sprites.rs
+++ rust/src/sprites.rs
@@ -9,6 +9,7 @@ use std::path::{Path,PathBuf};
 // use strum::AsStaticRef;
 
 extern crate sdl2;
+use sdl2::surface::Surface;
 use sdl2::image::LoadSurface;
 
 enum Action {
@@ -57,10 +58,22 @@ impl ActionImages {
  * get returned in a Vec<PathBuf>.
  */
 // TODO - make this return validated File objects.
-fn get_actionimages_file_list(path: &Path) -> io::Result<Vec<PathBuf>> {
+fn get_actionimages(path: &Path) -> io::Result<Vec<Surface>> {
     let path = current_dir()?.as_path().join(path);
-    let entries = read_dir(path)?;
-    Ok(entries.filter_map(|s| Some(s.ok().unwrap().path().to_path_buf())).collect())
+    let entries = read_dir(path)?.filter_map(|s| Some(s.ok().unwrap().path()));
+    
+    /* Load the surfaces from file. */
+    let mut ais: Vec<Surface> = Vec::new();
+    for f in entries {
+        let surface: Surface = match LoadSurface::from_file(&f) {
+            Ok(s) => s,
+            Err(_) => { continue },
+        };
+
+        ais.push(surface);
+    }
+
+    Ok(ais)
 }
 
 
@@ -75,9 +88,10 @@ mod tests {
         let assassin = ActionImages::new("assassin".to_string(), path);
         assert_eq!(assassin.name, "assassin".to_string());
         // assert_eq!(assassin.action, Action::Idle);
-        if get_actionimages_file_list(path).is_err() {
-            println!("{}", get_actionimages_file_list(path).unwrap_err());
-        }
+        let ai = get_actionimages(path).unwrap();
+        for a in ai {
+            println!("{:?}", a.size());
+        }        
     }
 
 
